96a34ca2835b617963f375c331b735c08131c9d9
Aleksey Safonov <55020240+safal207@users.noreply.github.com>
2026-05-25 10:01:13 +0200

    fix: simplify lpictl jest config - remove test exclude from tsconfig
    Instead of separate tsconfig.test.json, just remove **/*.test.ts
    from exclude so ts-jest can find jest globals naturally.

--- packages/lpictl/jest.config.js
+++ packages/lpictl/jest.config.js
@@ -3,11 +3,6 @@ module.exports = {
   testEnvironment: 'node',
   roots: ['<rootDir>/src'],
   testMatch: ['**/__tests__/**/*.test.ts'],
-  transform: {
-    '^.+\\.ts$': ['ts-jest', {
-      tsconfig: 'tsconfig.test.json',
-    }],
-  },
   collectCoverageFrom: [
     'src/**/*.ts',
     '!src/**/*.d.ts',
