870ab7081b2dd87f56361a28f1703f47098e6ea3
Aleksey Safonov <55020240+safal207@users.noreply.github.com>
2026-05-25 12:06:16 +0300

    migrate to eslint flat config for eslint v10 compatibility

---
+++ packages/node-lri/eslint.config.mjs
@@ -0,0 +1,38 @@
+import tsParser from '@typescript-eslint/parser';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
+import js from '@eslint/js';
+import globals from 'globals';
+
+export default [
+  js.configs.recommended,
+  ...tsPlugin.configs['flat/recommended'].slice(0, 2),
+  {
+    files: ['src/**/*.ts'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: 'module',
+      },
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+        ...globals.es2021,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      ...tsPlugin.configs['flat/recommended'][2].rules,
+      'preserve-caught-error': 'off',
+      'no-useless-assignment': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    },
+  },
+  {
+    ignores: ['dist/', 'node_modules/', '**/__tests__/**'],
+  },
+];
